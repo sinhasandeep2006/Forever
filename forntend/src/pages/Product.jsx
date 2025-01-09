5c35071fcb643ed437e3efd34d551cda99094203
sinha <1233sandeepsinha@gmail.com>
2025-01-09 23:40:08 +0530

    make the product page

--- forntend/src/pages/Product.jsx
+++ forntend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import ReleatedProduct from "../components/ReleatedProduct";
 
 const Product = () => {
   const { productId } = useParams();
@@ -67,9 +68,29 @@ const Product = () => {
             </div>
           </div>
           <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"> ADD to Cart</button>
-        <hr className=""/>
+        <hr className="mt-8 sm:w-4/5"/>
+        <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+          <p>100% Original Product.</p>
+          <p>Cash on delivery is available on this product.</p>
+          <p>Easy return and exchange policy withi 7 days</p> 
+          </div>
         </div>
       </div>
+      {/*  ------------------Description and Review Section-----------*/}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews(122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur nisi dolorem eius animi hic consectetur ratione molestiae doloribus accusamus tempora earum impedit cum delectus, deserunt ut consequuntur est similique perspiciatis suscipit corporis nemo facilis non eum sit!</p>
+          <p className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi quae voluptas mollitia inventore officia ad necessitatibus reiciendis, soluta autem suscipit ipsum magnam voluptatibus. Qui porro quod itaque dolorem officia voluptas.</p>
+          </div> 
+      </div>
+      {/* ----- Dispaly related products */}
+      <ReleatedProduct 
+      category={ProductData.category}
+      subCategory={ProductData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
