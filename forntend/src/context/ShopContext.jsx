8d1c4d79f886b661e05300224ad19428aa286973
sinha <1233sandeepsinha@gmail.com>
2025-01-10 19:07:41 +0530

    e-commerce

--- forntend/src/context/ShopContext.jsx
+++ forntend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext()
@@ -9,11 +9,30 @@ const ShopContextProvider =(props)=>{
 
     const[search ,setSearch]=useState('');
     const [showSearch,setShowsearch] =useState(false)
+    const [ cartItems, setcartItems]=useState({})
+    const addToCart= async (itemId,size)=>{
+        let cartData=  structuredClone(cartItems);
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]+=1
+            }
+            else{
+                cartData[itemId][size]=1
+            }
+        }
+        else{
+            cartData[itemId]={};
+            cartData[itemId][size]=1
+            setcartItems(cartData)
+        }
+    }
     const value={  
         products
-        ,currency,delivery_fee,search,setSearch,showSearch,setShowsearch
+        ,currency,delivery_fee,search,setSearch,showSearch,setShowsearch,cartItems,addToCart
     }
-
+    useEffect(()=>{
+        
+    },[cartItems])
     return (
         <ShopContext.Provider value={value}>
             {props.children}
--- forntend/src/pages/Product.jsx
+++ forntend/src/pages/Product.jsx
@@ -6,7 +6,7 @@ import ReleatedProduct from "../components/ReleatedProduct";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products,currency } = useContext(ShopContext);
+  const { products,currency ,addToCart} = useContext(ShopContext);
   const [ProductData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, SetSize] = useState("");
@@ -56,7 +56,7 @@ const Product = () => {
             <p className="pl-2">(122)</p>
           </div>
           <p className="mt-5 text-3xl font-medium">{currency}{ProductData.price}</p>
-          <p className="mt-5 text-gray-500 md-w-4/5">{ProductData.description}</p>
+          <p className="mt-5 text-gray-500 md:w-4/5">{ProductData.description}</p>
           <div className="flex flex-col gap-4 my-8">
             <p>Select Size</p>
             <div className="flex gap-2">
@@ -67,7 +67,7 @@ const Product = () => {
               }
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"> ADD to Cart</button>
+          <button onClick={()=>addToCart(ProductData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"> ADD to Cart</button>
         <hr className="mt-8 sm:w-4/5"/>
         <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
           <p>100% Original Product.</p>
