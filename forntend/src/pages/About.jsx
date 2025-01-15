5ce53621394b1fd17e5d7bf889d47f5577a239de
sinha <1233sandeepsinha@gmail.com>
2025-01-15 23:30:19 +0530

    complete front end

--- forntend/src/pages/About.jsx
+++ forntend/src/pages/About.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import Title from './../components/Title';
+import { assets } from './../assets/frontend_assets/assets';
+import NewLetter from './../components/NewLetter';
 const About = () => {
   return (
     <div>
-      
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={'ABOUT'} text2={' US'}/>
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img src={assets.about_img} alt="" className="w-full md:max-w-[450px]" />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum nam eveniet pariatur quis quo nihil iste quod aut repudiandae velit, nesciunt ducimus voluptatem amet nemo magni, ut molestiae sunt quibusdam possimus mollitia, quam aliquid dicta. Ad sunt debitis a, at, doloribus, totam quos architecto blanditiis minima voluptates consectetur impedit.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure reprehenderit accusamus tenetur minima labore eum ex, magnam aperiam velit et facilis. At necessitatibus ducimus provident dolores hic! Fuga laboriosam minus, corrupti voluptatibus asperiores minima nobis eius doloremque saepe ducimus nulla, suscipit praesentium at delectus vel provident quidem quisquam adipisci quis.</p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste minima animi eius expedita quo porro accusamus qui eligendi eos architecto?</p>
+        </div>
+        </div>
+        <div className="text-4xl py-4">
+          <Title text1={"why"} text2={"Chosose Us"}/>
+        </div>
+        <div className="flex flex-col md:flex-row text-sm mb-20">
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance</b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga placeat quaerat minus exercitationem ex qui facilis commodi quidem asperiores!</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convienve</b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga placeat quaerat minus exercitationem ex qui facilis commodi quidem asperiores!</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exception customer service </b>
+            <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis fuga placeat quaerat minus exercitationem ex qui facilis commodi quidem asperiores!</p>
+          </div>
+        </div>
+        <NewLetter/>
     </div>
   )
 }
--- forntend/src/pages/Contact.jsx
+++ forntend/src/pages/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewLetter from './../components/NewLetter';
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={'CONTACT'} text2={' US'}/>
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img src={assets.contact_img} alt="" className="w-full md:max-w-[480px]" />
+          <div className="flex flex-col justify-center items-start gap-6">
+            <p className="font-semibold text-xl text-gray-500">Our Store</p>
+            <p className="text0text-gray-500">34233 willms station <br /> suit 345 , was,Uk</p>
+            <p className="text-gray-500">tel:(33) 3939 399 <br />email:1233sadndeee@gmail.com</p>
+            <p className="font-semibold text-gray-600">careers at forever</p>
+            <p className="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <button className='border border-black text-black text-sm px-8 py-2 mt-4 hover:bg-black hover:text-white transition-all duration-500' > explor Jobs</button>
+          </div>
+        
+      </div>
+      <NewLetter/>
     </div>
   )
 }
