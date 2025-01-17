f8c35b76ddf12a0aa27cfd47ba868767c4893ee3
sinha <1233sandeepsinha@gmail.com>
2025-01-17 17:11:08 +0530

    backend connect

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+
+//Route for user login
+const loginUser =async(req,res)=>{
+
+}
+
+//Route for user Register
+const registerUser =async(req,res)=>{
+res.json({
+    msg:"api working"
+})
+} 
+//Route for admin login
+const adminLogin =async(req,res)=>{
+
+} 
+export {loginUser,registerUser,adminLogin}
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+
+//app config 
+
+const app = express()
+const Port=process.env.PORT || 5000
+connectDB()
+connectCloudinary()
+
+//middleware
+
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.use('/api/user',userRouter)
+app.get('/',(req,res)=>{
+    res.send(
+        "API WORKING"
+    )
+})
+
+app.listen(Port,()=>console.log('server started on Port : '+Port));
